9ae8d94836feee0e8e0265941055808a20a29216
Tomoyoshi Iwamoto <79735428+tomo0714@users.noreply.github.com>
2021-10-27 21:21:59 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Function
+const highlightMenu = () => {
+    for(let i = 1; i <= 4; i++) {
+        // DOM
+        const section = document.querySelector(`main section:nth-child(${i})`);
+        const list = document.querySelector(`nav ul li:nth-child(${i})`);
+        const lists = Array.from(document.querySelectorAll('nav ul li'));
+
+        // constant
+        const gap = 300;
+
+        // hithlight
+        if(section.offsetTop - gap < window.scrollY) {
+            lists.forEach(li => {
+                li.classList.remove('current');
+            });
+            list.classList.add('current');
+        }
+    }
+};
+
+// Event
+window.addEventListener('scroll', highlightMenu);
